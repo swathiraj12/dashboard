4fb7db9939c86f18420374e6e6804505b94f6ef2
swathiraj12 <swathijcvl@gmail.com>
2024-10-07 10:16:36 +0530

    first commit

---
+++ dashboard/src/App.jsx
@@ -0,0 +1,12 @@
+import Card from "./Components/Card";
+import Home from "./Components/Home";
+
+let App = () => {
+    return(
+        <div>
+            <Home />
+            <Card />
+        </div>
+    )
+};
+export default App;
