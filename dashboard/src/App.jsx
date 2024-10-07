c452ee0d49daab55a4edf62c32606d76d0480c03
swathiraj12 <swathijcvl@gmail.com>
2024-10-07 16:44:05 +0530

    final changes

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
-=======
-import Card from "./Components/Card";
->>>>>>> 4678d01ba799f16853baf0ecc2b25f9e5af795ee
 import Home from "./Components/Home";
 
 let App = () => {
     return(
         <div>
             <Home />
-<<<<<<< HEAD
-=======
-            <Card />
->>>>>>> 4678d01ba799f16853baf0ecc2b25f9e5af795ee
         </div>
     )
 };
